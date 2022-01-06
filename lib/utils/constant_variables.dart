d011b0070acecc8624b404bd68134f379418f115
Darylabrador <abrador.daryl@gmail.com>
2022-01-06 10:44:12 +0400

    update

--- lib/utils/constant_variables.dart
+++ lib/utils/constant_variables.dart
@@ -1,4 +1,3 @@
 class ConstantVariables {
-  static const startingURL = "http://localhost:8000/api";
-  // static const startingURL = "https://api.hackathon.run";
+  static const startingURL = "https://c4c8-2a01-cb22-916-f700-95c5-bd84-67d8-cf24.ngrok.io/api";
 }
